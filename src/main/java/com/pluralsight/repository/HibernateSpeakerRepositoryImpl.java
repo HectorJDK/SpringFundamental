872114d8cf9a33e0a6f41288495b4e9bf7f33047
Hector De La Garza <hector.hdp90@gmail.com>
2022-08-09 12:38:56 -0500

    Factory Beans

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     @Override
     public List<Speaker> findAll() {
@@ -18,6 +23,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setLastName("Garza");
         speakers.add(speaker);
 
+        System.out.println("cal: " + cal.getTime());
         return speakers;
     }
 }
